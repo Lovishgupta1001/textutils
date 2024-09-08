f4e6b0bf15a972f63a68417c935812a850578438
lovishgupta1001 <lovishgupta1001@gmail.com>
2024-09-08 18:25:36 +0530

    Alert Added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,13 +29,14 @@ export default function TexthtmlForm(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea
                         className="form-control"
                         value={text}
                         onChange={handleOnChange}
+                        style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? 'black' : 'white' }}
                         placeholder='Enter Text here'
                         id="myBox"
                         rows="8">
@@ -45,13 +46,13 @@ export default function TexthtmlForm(props) {
                 <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
                 <button className="btn btn-secondary mx-2" onClick={handleClearClick}>Clear Text</button>
                 <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-            </div>
-            <div className="container my-3">
+            </div >
+            <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h2>Your Text Summary</h2>
                 <p>{text.split(/\s+/).filter((element) => element.length !== 0).length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").filter((element) => element.length !== 0).length} Minutes Read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p style={{ fontStyle: "italic" }}>{text.length > 0 ? text : "Enter Something in the textbox to Preview it here"}</p>
             </div>
         </>
     );
